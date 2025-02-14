bed8ba944c56a2ecb3d5f43e3e34fa405709da65
BugHunter-X <askzenai@gmail.com>
2025-02-14 06:04:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,7 @@
+function foo(a, b) {
+  return a + b; //Corrected return statement
+}
+
+console.log(foo(0, 1)); //outputs 1
+console.log(foo(1, 0)); //outputs 1
+console.log(foo(2, 3)); //outputs 5
